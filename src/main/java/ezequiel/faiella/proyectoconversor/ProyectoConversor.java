dcb284449de1d36ffa21f5474a98ff7366756b73
Fayu <r826720@hotmail.com>
2023-05-01 09:43:31 -0300

    carga del proyecto

---
+++ src/main/java/ezequiel/faiella/proyectoconversor/ProyectoConversor.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ezequiel.faiella.proyectoconversor;
+
+/**
+ *
+ * @author ezequiel
+ */
+public class ProyectoConversor {
+
+    public static void main(String[] args) {
+        VentanaPrincipal.main(args);
+        //System.out.println("Hello World!");
+    }
+}
